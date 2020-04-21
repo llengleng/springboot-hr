a731bfb50fb5e89202e4db873cae5eec062fd89c
llengleng <1132266959@qq.com>
2020-04-21 19:51:19 +0800

    添加swagger

--- src/main/java/com/niit/springboothr/controller/LoginController.java
+++ src/main/java/com/niit/springboothr/controller/LoginController.java
@@ -1,6 +1,10 @@
 package com.niit.springboothr.controller;
 
 import com.niit.springboothr.model.RespBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @description :
  */
 @RestController
+@Api(tags = {"登录 API"},protocols = "http")
+@SwaggerDefinition(tags = {
+        @Tag(name = "登录 API",description = "微人事登录的API接口")
+})
 public class LoginController {
+
+
     @GetMapping("/login")
     public RespBean login(){
         return RespBean.error("未登录，请登录");
