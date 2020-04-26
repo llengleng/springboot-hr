2732bbe74fc052a1c17c08b6c522122fc21e7f60
llengleng <1132266959@qq.com>
2020-04-26 17:06:44 +0800

    完成职位管理的增删改查功能

---
+++ src/main/java/com/niit/springboothr/mapper/PositionMapper.java
@@ -0,0 +1,25 @@
+package com.niit.springboothr.mapper;
+
+import com.niit.springboothr.model.Position;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface PositionMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Position record);
+
+    int insertSelective(Position record);
+
+    Position selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Position record);
+
+    int updateByPrimaryKey(Position record);
+
+    @Select("select * from position")
+    List<Position> selectAllPosition();
+
+    int deletePositions(Integer[] ids);
+}
---
+++ src/main/java/com/niit/springboothr/model/Position.java
@@ -0,0 +1,21 @@
+package com.niit.springboothr.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+
+@Data
+public class Position {
+    private Integer id;
+
+    private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:hh",timezone = "Asia/Shanghai")
+    private Date createdate;
+
+    private Boolean enabled;
+
+
+}
